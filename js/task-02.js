1e15d8ad505ffa994f894fe00af6b6e03697b6a9
Elegantniy <maxim9083@gmail.com>
2023-01-26 19:24:30 +0200

    update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,12 @@ const ingredients = [
 ];
 const ingredientsList = document.querySelector("#ingredients");
 const listItems = [];
-for (let i = 0; i < ingredients.length; i += 1) {
-  const ingredientsEl = document.createElement("li");
-  ingredientsEl.classList.add("item");
-  ingredientsEl.textContent = ingredients[i];
-  listItems.push(ingredients[i]);
+
+for (const ingredient of ingredients) {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.classList.add('item');
+  ingredientsItem.textContent = ingredient
+  listItems.push(ingredientsItem);
+  
 }
 ingredientsList.append(...listItems);
